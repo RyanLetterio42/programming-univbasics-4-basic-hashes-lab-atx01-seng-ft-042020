44815ce11929e15fb6647b0ac4e12845ffd84cb1
Ryan Letterio <rletterio42@gmail.com>
2020-04-11 21:11:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -3,14 +3,14 @@ def new_hash
 end
 
 def my_hash
-  my_new_has = {"name"
+  my_new_hah = {"number" => 1}
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer_hash = {:name => 'Grace Hopper'}
 end
 
 
 def id_hash_generator(number)
-  # return a hash with a key :id assigned to the provided number
+  id_hash = {:id => number}
 end

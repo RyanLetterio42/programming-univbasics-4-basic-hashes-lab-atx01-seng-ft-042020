7edd4f0918435394626457ec8017382890cc6fc6
Ryan Letterio <rletterio42@gmail.com>
2020-04-11 21:09:02 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,9 @@
 def new_hash
-  # return an empty hash
+  empty_hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_new_has = {"name"
 end
 
 def pioneer
